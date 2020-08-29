b2b69b6b967ca344baca0e8c63636e9f2d7ef5c2
chidibede <chidibede@gmail.com>
2020-08-29 20:48:16 +0100

    attempt at fixing server error in production

--- src/server.ts
+++ src/server.ts
@@ -3,36 +3,20 @@ import { ApolloServer } from "apollo-server-express";
 
 const app: Application = express();
 
-if (process.env.NODE_ENV === "development") {
-  const apolloServer = new ApolloServer({
-    modules: [
-      require("./graphql/version_1/modules/business/index"),
-      require("./graphql/version_1/modules/businessCategory/index"),
-      require("./graphql/version_1/modules/auth/index"),
-    ],
-    context: ({ req }) => {
-      // get the authorization from the request headers and return the auth if there is
-      const auth = req.headers.authorization || "";
-      return { auth };
-    },
-  });
-  apolloServer.applyMiddleware({ app, path: "/graphql" });
-} else {
-  const apolloServer = new ApolloServer({
-    modules: [
-      require("./graphql/version_1/modules/business/index"),
-      require("./graphql/version_1/modules/businessCategory/index"),
-      require("./graphql/version_1/modules/auth/index"),
-    ],
-    playground: true,
-    context: ({ req }) => {
-      // get the authorization from the request headers and return the auth if there is
-      const auth = req.headers.authorization || "";
-      return { auth };
-    },
-  });
-  apolloServer.applyMiddleware({ app, path: "/graphql" });
-}
+const apolloServer = new ApolloServer({
+  modules: [
+    require('./graphql/version_1/modules/business/index'),
+    require('./graphql/version_1/modules/businessCategory/index'),
+    require('./graphql/version_1/modules/auth/index')
+  ],
+  context: ({ req }) => {
+    // get the authorization from the request headers and return the auth if there is
+    const auth = req.headers.authorization || "";
+    return { auth };
+  },
+  playground: true
+});
+apolloServer.applyMiddleware({ app, path: "/graphql" });
 
 app.set("view engine", "html");
 app.get("/", (req, res) => {
