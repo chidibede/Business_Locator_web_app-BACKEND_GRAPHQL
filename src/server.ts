3ba43be3df176b49716f3778971ec00c59ebc7d7
chidibede <chidibede@gmail.com>
2020-08-29 11:56:18 +0100

    fixed missing public folder

--- src/server.ts
+++ src/server.ts
@@ -29,11 +29,5 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  if (port === 3000){
-    console.log(`Server running on http://localhost:${port}/graphql`);
-  }else{
-    console.log(`Server running on port ${port}`);
-    
-  }
-  
+  console.log(`Server running on http://localhost:${port}/graphql`);
 });
